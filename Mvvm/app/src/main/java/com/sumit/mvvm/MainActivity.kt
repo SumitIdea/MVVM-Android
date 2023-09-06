efb8315ea61dda022206d677b670270c5afbae8a
SumitIdea <sumitidea23jun@gmail.com>
2023-09-06 11:56:22 +0530

    MVVM Kotlin in Android

---
+++ Mvvm/app/src/main/java/com/sumit/mvvm/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.sumit.mvvm
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.sumit.mvvm.databinding.ActivityMainBinding
+import com.sumit.mvvm.view.adapter.MovieAdapter
+import com.sumit.mvvm.viewmodels.MovieViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MovieViewModel
+    private lateinit var movieAdapter : MovieAdapter
+
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prepareRecyclerView()
+        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+                viewModel.getPopularMovies()
+        viewModel.observeMovieLiveData().observe(this, Observer { movieList ->
+            movieAdapter.setMovieList(movieList)
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.rvMovies.apply {
+            layoutManager = GridLayoutManager(applicationContext,2)
+            adapter = movieAdapter
+        }
+    }
+
+
+}
---
+++ Mvvm/app/src/main/java/com/sumit/mvvm/data/api/Api.kt
@@ -0,0 +1,11 @@
+package com.sumit.mvvm.data.api
+
+import com.sumit.mvvm.data.entity.Movies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    @GET("popular?")
+    fun getPopularMovies(@Query("api_key") api_key : String) : Call<Movies>
+}
